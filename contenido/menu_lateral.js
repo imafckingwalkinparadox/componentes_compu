9639684ec3f7eceabd6d41514f1c7fdf09ad3a03
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-05-08 15:56:21 -0600

    version0.3

---
+++ contenido/menu_lateral.js
@@ -0,0 +1,10 @@
+let div_menu = document.querySelector(".menu_lateral")
+div_menu.innerHTML = `
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="CPU.html">CPU</a> </nav> </div> </div>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="GPU.html">GPU</a> </nav> </div> </div>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="TM.html">Tarjeta Madre</a> </nav> </div> </div>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="RAM.html">RAM</a> </nav> </div> </div>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="SSD.html">SSD</a> </nav> </div> </div>
+<div class= "cuadros"> <div class= "menus"> <nav> <a href="SIM.html">Tarjeta SIM</a> </nav> </div> </div>`;
+
+ 

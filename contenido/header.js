7dc573505672b0fd86f6bfd22777d2aff9d6ac46
andreecalderon <jacalderon@scl.edu.gt>
2024-05-09 10:09:50 -0600

    correccion header

--- contenido/header.js
+++ contenido/header.js
@@ -5,8 +5,7 @@ li_jcalderon.innerHTML=`
     <img src="https://www.codigocorrecto.com/wp-content/uploads/2020/01/componentes-compressor-1024x536.jpg" alt="">
 </div>
 
-<div>
-    <p>menu principal</p>
+<div class="m">
     <p>servicios</p>
     <p>contactos</p>
     <p>ajustes</p>

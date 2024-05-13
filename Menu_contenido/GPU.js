7866cf184c9407bb78ad2c47349f829a07d84ea9
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-05-13 16:40:37 -0600

    new version

---
+++ Menu_contenido/GPU.js
@@ -0,0 +1,4 @@
+let div_cpu = document.querySelector("GPU")
+div_cpu.innerHTML = `
+<div class="titulo_GPU">GPU</div> 
+<div class="p"><p>La unidad de procesamiento de gráficos (GPU) es un circuito electrónico que puede realizar cálculos matemáticos a alta velocidad. Algunas tareas de computación, como la renderización de gráficos, el machine learning (ML) y la edición de videos, requieren la aplicación de operaciones matemáticas similares en un conjunto de datos grande.</p></div>`;

be071c95e8219593dbd39961b9a2b72835e9a91b
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-05-14 08:39:36 -0600

    new version index

--- contenido/menu_lateral.js
+++ contenido/menu_lateral.js
@@ -1,10 +1,10 @@
 let div_menu = document.querySelector(".menu_lateral")
 div_menu.innerHTML = `
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="CPU.html">CPU</a> </nav> </div> </div> <br>
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="GPU.html">GPU</a> </nav> </div> </div> <br>
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="TM.html">Tarjeta Madre</a> </nav> </div> </div> <br>
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="RAM.html">RAM</a> </nav> </div> </div> <br>
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="SSD.html">SSD</a> </nav> </div> </div> <br>
-<div class= "cuadros"> <div class= "menus"> <nav> <a href="SIM.html">Tarjeta SIM</a> </nav> </div> </div>`;
+<div class= "cuadros"> <div class= "menus"> <a href="CPU.html">CPU</a> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <a href="GPU.html">GPU</a> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <a href="TM.html">Tarjeta Madre</a> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <a href="RAM.html">RAM</a> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <a href="SSD.html">SSD</a> </div> </div> <br>
+<div class= "cuadros"> <div class= "menus"> <a href="SIM.html">Tarjeta SIM</a> </div> </div>`;
 
  

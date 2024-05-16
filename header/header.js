83b180601322d2984f6e277aef8dc57023b9d12e
imafckingwalkinparadox <hdvillatoro@scl.edu.gt>
2024-05-16 10:25:01 -0600

    nueva configuracion

---
+++ header/header.js
@@ -0,0 +1,14 @@
+let li_jcalderon=document.querySelector(".header");
+
+li_jcalderon.innerHTML=`
+<div>
+    <img src="https://www.codigocorrecto.com/wp-content/uploads/2020/01/componentes-compressor-1024x536.jpg" alt="">
+</div>
+
+<div class="m">
+    <a href="">servicios</a>
+    <a href="">contactos</a>
+    <a href="">ajustes</a>
+    <a href="">menu principal</a>   
+</div>
+`;

4b0834de4c42558ba847abb541c8211b9a93528c
andreecalderon <jacalderon@scl.edu.gt>
2024-05-16 20:29:19 -0600

    footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,13 +1,20 @@
 let calderon=document.querySelector(".footer");
 
 calderon.innerHTML=`
-<div>
-    <img src="https://static.wixstatic.com/media/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png/v1/fit/w_2500,h_1330,al_c/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png" alt="">
+<div class="r4">
+    <img src="https://static.wixstatic.com/media/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png/v1/fit/w_2500,h_1330,al_c/d1b317_4fe70cb66f9447c3991ad4cb8d7294d3~mv2.png" alt="" class="imgx">
+
+    <div class="a1">
+        <a href="" class="m1">facebook</a>
+        <a href="" class="m3">contacto📞</a>
+    </div>
+    
+    <div class="mt3">
+        <p class="r3">dirección 12-20 zona 1 de mixco, colegio Santa Catalina Laboure</p>
+        <p class="r2">pagina programada el 16/05/2024</p>
+    </div>
+    
 </div>
 
-<div>
-    <a href="">facebook</a>
-    <a href="">intagram</a>
-    <a href="">contacto📞</a>
-</div>
+
 `;
